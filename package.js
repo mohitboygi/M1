ebda32e7ca05e720631167eeb9b26e0030fd10de
mohitboygi <pra524729@gmail.com>
2026-08-25 11:56:48 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "ai-router",
+  "version": "1.0.0",
+  "description": "Backend router: Groq chat modes + Cloudflare/AI-Horde image gen + Pixazo/LTX video gen",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "dotenv": "^16.4.5",
+    "express": "^4.19.2"
+  },
+  "devDependencies": {
+    "nodemon": "^3.1.0"
+  }
+}
